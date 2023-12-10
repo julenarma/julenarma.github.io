6ded383eda40f1fccbc5a3e4c82ddbc2cb604ab4
julenarma <julenarmelex@gmail.com>
2023-12-11 00:08:46 +0100

    js main & index ordened

--- view/js/main.js
+++ view/js/main.js
@@ -32,3 +32,71 @@ function getRelativeImagePath(relativePath) {
 
     return prefix + relativePath;
 }
+
+
+
+//------mostrar cerrar modal---
+function mostrarModal() {
+    $('#modal').modal('show');
+  }
+  
+  function cerrarModal() {
+    $('#modal').modal('hide');
+  }
+  //---mostrar cerrar modal fin ---
+  
+  
+  //------------Donacion cafe start-------
+  function realizarSupport() {
+    // Lógica de pago
+    var cantidadSeleccionada = getCantidadSeleccionada();
+    var mensaje = getMensaje();
+    var mensajeAlerta = 'Support realizado para ' + cantidadSeleccionada + ' cafés';
+  
+    if (mensaje) {
+      mensajeAlerta += ' con el mensaje: ' + mensaje;
+    }
+  
+    alert(mensajeAlerta);
+    cerrarModal();
+  }
+  
+  function seleccionarCantidad(cantidad) {
+    actualizarCantidadPersonalizada(cantidad);
+    actualizarTotalSoporte(cantidad);
+  }
+  
+  function getCantidadSeleccionada() {
+    return parseInt(document.getElementById('cantidadPersonalizada').value) || 0;
+  }
+  
+  function getMensaje() {
+    return document.getElementById('mensaje').value;
+  }
+  
+  function actualizarCantidadPersonalizada(cantidad) {
+    document.getElementById('cantidadPersonalizada').value = cantidad;
+  }
+  
+  function actualizarTotalSoporte(cantidad) {
+    document.getElementById('totalSoporte').innerText = cantidad;
+    actualizarTextoBoton(cantidad);
+  }
+  
+  function actualizarTextoBoton(cantidad) {
+    var botonSupport = document.getElementById('supportButton');
+    botonSupport.innerText = 'Support (' + cantidad + '€)';
+  }
+  
+  // Añadir evento input para validar la entrada en el campo de cantidad personalizada
+  document.getElementById('cantidadPersonalizada').addEventListener('input', function () {
+    var cantidadInput = this.value;
+    if (cantidadInput < 0) {
+      this.value = 0;
+    } else if (cantidadInput > 10) {
+      this.value = 10;
+    }
+  
+    actualizarTotalSoporte(this.value);
+  });
+  //------------Donacion cafe end-------
